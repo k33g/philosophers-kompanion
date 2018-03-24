5ccd420ef35878df126105019971620b281145d7
Sylvain Bonnard <sylvain@okamidev.com>
2018-03-24 10:40:32 +0100

    fix and add some quotes

--- src/main/kotlin/com/okamidev/philosopher/PhilosopherInteractor.kt
+++ src/main/kotlin/com/okamidev/philosopher/PhilosopherInteractor.kt
@@ -8,14 +8,19 @@ class PhilosopherInteractor {
                     "/img/moss.jpg",
                     listOf(
                             "I'm here to drink milk and kick ass. And i just finished my milk...",
-                            "I'll just put the over here with the rest of the fire."
+                            "I'll just put this over here with the rest of the fire.",
+                            "I like being weird, weird is all I’ve got. That and my sweet style"
                     )
             ),
             2 to Philosopher(2,
                     "Sheldon Cooper",
                     "/img/sheldon.jpg",
                     listOf(
-                            "This is my spot"
+                            "This is my spot",
+                            "For the record, it could kill us to meet new people. They could be murderers or the carriers of unusual pathogens. And I'm not insane, my mother had me tested.",
+                            "(3 knocks) Penny! (3 knocks) Penny! (3 knocks) Penny!",
+                            "Don't be silly, I'm a fan of anything that tries to replace actual human contact.",
+                            "The correct animal for inter-species super soldier is the koala. You would wind up with an army so cute, it couldn't be attacked."
                     )
             ),
             3 to Philosopher(3,
@@ -23,7 +28,8 @@ class PhilosopherInteractor {
                     "/img/goldblum.jpg",
                     listOf(
                             "La vie trouve toujours le chemin.",
-                            "Dieu crée les dinosaures. Dieu détruit les dinosaures. Dieu crée l’homme. L’homme détruit Dieu. L’homme crée les dinosaures..."
+                            "Dieu crée les dinosaures. Dieu détruit les dinosaures. Dieu crée l’homme. L’homme détruit Dieu. L’homme crée les dinosaures...",
+                            "See, here I'm now sitting by myself, uh, er, talking to myself. That's, that's chaos theory"
                     )
             ),
             4 to Philosopher(
@@ -32,7 +38,8 @@ class PhilosopherInteractor {
                     "/img/jcvd.jpg",
                     listOf(
                             "Une noisette, je la casse entre mes fesses!",
-                            "1+1 = 11"
+                            "Moi, Adam et Eve, j’y crois plus tu vois, parce que je suis pas un idiot : la pomme ça peut pas être mauvais, c’est plein de pectine…",
+                            "Selon les statistiques, il y a une personne sur cinq qui est déséquilibrée. S'il y a 4 personnes autour de toi et qu'elles te semblent normales, c'est pas bon."
                     )
             )
     )
